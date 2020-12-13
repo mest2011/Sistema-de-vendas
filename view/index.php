c39211a3fa6b3c0e25cfffde6f96f4a87e45f93c
Miguel Eto Sobral Teixeira <mest2011mest@gmail.com>
2020-12-13 17:48:14 -0300

    Start project

---
+++ view/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vendas</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+        <div>logo</div>
+        <ul>
+            <li>Cadastrar</li>
+            <li>...</li>
+            <li>...</li>
+        </ul>
+    </header>
+    <main>
+        <table class="table table-striped">
+            <thead class="thead-dark">
+                <th scope="col">Cliente</th>
+                <th scope="col">Produto</th>
+                <th scope="col">Valor uni.</th>
+                <th scope="col">qtd</th>
+                <th scope="col">Valor total</th>
+                <th scope="col">Endereço</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                </tr>
+                <tr>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                </tr>
+                <tr>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                </tr>
+                <tr>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                </tr>
+                <tr>
+                    <td colspan="6">Valor total:</td>
+                </tr>
+            </tbody>
+            <tfoot>
+            </tfoot>
+        </table>
+    </main>
+    <footer></footer>
+</body>
+</html>
